70b22b1c62232a759bc7c0c9da3d3d794ce96d9b
t_kyn <153400466+t-kyn-git@users.noreply.github.com>
2025-09-06 13:55:03 +0000

    update the back button and all start and stop

---
+++ content/t_kyn_test_app/all_manage_t_kyn_stop.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# all_manage_t_kyn_stop.sh
+cd /workspaces/t_kyn_communal_the_test/content/t_kyn_test_app;pwd
+echo "=====run the app server====="
+ps -ef | grep manage_t_kyn.py | awk -F ' ' '{print $2}' | xargs kill -9
+echo "=====run the app server done====="
